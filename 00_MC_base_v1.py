c4ed18cb69361aaa515b480cc8f892856793847d
Kgee321 <geek@middleton.school.nz>
2023-05-04 15:17:24 +1200

    Update 00_MC_base_v1.py

--- 00_MC_base_v1.py
+++ 00_MC_base_v1.py
@@ -14,9 +14,89 @@ Created on the 1/05/2023
 import easygui
 
 
-# Adding a new card function
+# Function for joinning dictionary's
+def joinning(dic):
+    message = ""
+
+    # Joinning the dictionary inputted
+    for key, value in dic.items():
+        message = f"{key}:\n"
+
+        # Formatting dictionary inside dictionary
+        for key2, value2 in value.items():
+            message += f"- {key2}: {value2} \n"
+
+    return message
+
+
+# Function for checking characters stay in letter boundary
+def char_boundary(lower, upper, question_message, box):
+    # Loop for character checker
+    while True:
+        # Asking user to enter monster card name
+        question = easygui.enterbox(question_message, box).title()
+
+        # Upper string boundary
+        if len(question) > upper:
+            easygui.msgbox("Wrong Input. Please enter less than 20 letters",
+                           "Number to high")
+
+        # Lower string boundary
+        elif len(question) < lower:
+            easygui.msgbox("Wrong Input. Please more than 1 letters",
+                           "Number to low")
+
+        # Input correct
+        else:
+            return question
+
+
+# Function for adding a new Monster Card
 def add():
-    pass
+
+    # Variable setting
+    attributes = ["Strength", "Speed", "Stealth", "Cunning"]
+    ability_level = {}
+    new_card = {}
+
+    # Asking user to enter monster card name with character checker
+    card_name = char_boundary(1, 20,
+                              "Please enter your chosen Monster Card name: ",
+                              "Card Name")
+
+    # Loop for the 4 attributes
+    for item in attributes:
+        # Asking user to enter how much strength card has
+        number = easygui.integerbox(f"What is {card_name}'s level of {item}? \n"
+                                    f"(Number between 1 and 25) \n",
+                                    f"{item}", upperbound=25,
+                                    lowerbound=1)
+
+        # Adding items and number to as the ability
+        ability_level[item] = number
+
+    # Adding ability's and monster card name to a new dictionary
+    new_card[card_name] = ability_level
+
+    # Joinning code for monster cards dictionary
+    new_card_formatted = joinning(new_card)
+
+    # Output results and asking if correct results
+    answer = easygui.buttonbox("The new Monster Card is: \n\n"
+                               f"{new_card_formatted} \n\n"
+                               f"Is this new Monster Card correct?",
+                               choices=["Yes", "No"])
+
+    # User enters yes
+    if answer == "Yes":
+        easygui.msgbox("Great! This New Combo was added", "New Combo Added")
+        monster_cards.update(new_card)
+        print(monster_cards)
+
+    # User enters no
+    elif answer == "No":
+        easygui.msgbox("So Sorry! Lets make some changes",
+                       "New Card incorrect")
 
 
 # Searching the cards functions
@@ -125,5 +205,6 @@ while True:
     elif options == "Exit":
 
         # Goodbye message outputted and code ends
-        easygui.msgbox("Goodbye! Thanks for using Monster Cards", "Leaving Screen")
+        easygui.msgbox("Goodbye! Thanks for using Monster Cards",
+                       "Leaving Screen")
         break
