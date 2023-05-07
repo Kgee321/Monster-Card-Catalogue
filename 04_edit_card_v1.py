b1c4f725406ee43580a90ecd9cbc1056d7e04ac6
Kgee321 <geek@middleton.school.nz>
2023-05-07 17:03:49 +1200

    Update 04_edit_card_v1.py

--- 04_edit_card_v1.py
+++ 04_edit_card_v1.py
@@ -3,30 +3,14 @@ Adding a monster card into use as card to edit
 Code should ask what player wants to edit
 Going straight to using easygui as it will
 be hard and unnecessary to use print statements
-for asking what part of the card they want to
+for asking what part of the card user wants to
 edit.
-Adding the joinning function so dictionary can be printed
+Starting by only using name and nothing button
 Written by Katelyn Gee
 Created on the 4/05/2023
 """
 
 import easygui
-
-
-# Function for joinning dictionary's
-def joinning(dic):
-    message = ""
-
-    # Joinning the dictionary inputted
-    for key, value in dic.items():
-        message = f"{key}:\n"
-
-        # Formatting dictionary inside dictionary
-        for key2, value2 in value.items():
-            message += f"- {key2}: {value2} \n"
-
-    return message
-
 
 # A monster card
 edit_card = {
@@ -37,25 +21,35 @@ edit_card = {
          "Cunning": 15},
 }
 
-# Calling on join function
-edit_card_formatted = joinning(edit_card)
+# Loop for editing the card
+while True:
 
-# Asking what user wants to edit
-what_edit = easygui.buttonbox("What part of this card would you like to edit: \n"
-                              f"{edit_card_formatted}", "Editing card",
-                              choices=["Name", "Strength", "Speed",
-                                       "Stealth", "Cunning",
-                                       "Nothing: This card is correct"])
+    # Asking what user wants to edit
+    what_edit = easygui.buttonbox("What part of this card would you like to edit: \n"
+                                  f"{edit_card}", "Editing card",
+                                  choices=["Name", "Strength", "Speed",
+                                           "Stealth", "Cunning",
+                                           "Nothing"])
 
+    # Loop for access dictionary values
+    for original_name, original_value in edit_card.items():
 
-for edit_name, edit_values in edit_card.items():
+        # Changing name
+        if what_edit == "Name":
 
-    # Changing name
-    if what_edit == "Name":
+            # Asking what user want to change name to
+            edit_name = easygui.enterbox("Enter the new card name: ",
+                                         "Editing card name").title()
 
-        # Asking what user want to change name to
-        edit_name = easygui.enterbox("Enter the new card name: ",
-                                     "Editing card name")
+            # Adding name change to final dictionary
+            edit_card[edit_name] = original_value
 
-        # Changing name in combo
-        edit_card[edit_name] = edit_values
+            # Deleting old card and ending loop
+            del edit_card[original_name]
+            break
+
+    # If card not changing
+    if what_edit == "Nothing":
+        # Happy message
+        easygui.msgbox("Ok!", "No changes needed")
+        break
