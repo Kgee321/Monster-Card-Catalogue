3e4497692816b0b20e490985f248f5b96c687c97
Kgee321 <geek@middleton.school.nz>
2023-05-09 21:24:50 +1200

    Create 07_search_combos_v1.py

---
+++ 07_search_combos_v1.py
@@ -0,0 +1,74 @@
+""" Component 4 -- Search card (version 1)
+Asks user to enter what they want to search for
+Outputs the card that is in the search or an
+error if the search is not a card.
+Full pre saved dictionary added
+Written by Katelyn Gee
+Created on the 09/05/2023
+"""
+
+# Pre Stored Monster Cards dictionary
+monster_cards = {
+    "Stoneling":
+        {"Strength": 7,
+         "Speed": 1,
+         "Stealth": 25,
+         "Cunning": 15},
+    "Vexscream":
+        {"Strength": 1,
+         "Speed": 6,
+         "Stealth": 21,
+         "Cunning": 19},
+    "Dawnmirage":
+        {"Strength": 5,
+         "Speed": 15,
+         "Stealth": 18,
+         "Cunning": 22},
+    "Blazegolem":
+        {"Strength": 15,
+         "Speed": 20,
+         "Stealth": 23,
+         "Cunning": 6},
+    "Websnake":
+        {"Strength": 7,
+         "Speed": 15,
+         "Stealth": 10,
+         "Cunning": 5},
+    "Moldvine":
+        {"Strength": 21,
+         "Speed": 18,
+         "Stealth": 14,
+         "Cunning": 5},
+    "Vortexwing":
+        {"Strength": 19,
+         "Speed": 13,
+         "Stealth": 19,
+         "Cunning": 2},
+    "Rotthing":
+        {"Strength": 16,
+         "Speed": 7,
+         "Stealth": 4,
+         "Cunning": 12},
+    "Froststep":
+        {"Strength": 14,
+         "Speed": 14,
+         "Stealth": 17,
+         "Cunning": 4},
+    "Wispghoul":
+        {"Strength": 17,
+         "Speed": 19,
+         "Stealth": 3,
+         "Cunning": 2}
+}
+
+# User enters search
+searching = input("What Monster Card are you looking for? ").title()
+
+# Finding the input in dictionary
+if searching in monster_cards.keys():
+    print(f"The Monster Card named {searching} has {monster_cards[searching]}")
+
+# If searching not in dictionary
+else:
+    print("Sorry, input is not a Monster Card name")
+
