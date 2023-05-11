34b5552baba635a416e7814ac4d2badcbada8c87
Kgee321 <geek@middleton.school.nz>
2023-05-11 15:17:02 +1200

    Create 07_search_cards_v5.py

---
+++ 07_search_cards_v5.py
@@ -0,0 +1,129 @@
+""" Component 4 -- Search Cards (version 5)
+Removing testing loop from version 4
+Converting to easygui
+Asking user if card is correct or not
+Changing char_boundary to use easygui
+Adding in the joinning function for neat
+dictionary output
+Written by Katelyn Gee
+Created on the 11/05/2023
+"""
+
+import easygui
+
+
+# Function for joinning dictionary's
+def joinning(dic):
+    message = ""
+
+    # Joinning the dictionary inputted
+    for key, value in dic.items():
+        message += f"{key}:\n"
+
+        # Formatting dictionary inside dictionary
+        for key2, value2 in value.items():
+            message += f"- {key2}: {value2} \n"
+
+    return message
+
+
+# Function for checking characters stay in letter boundary
+def char_boundary(lower, upper, question_message, box):
+    # Loop for character checker
+    while True:
+        # Asking user to enter monster card name
+        question = easygui.enterbox(question_message, box)
+
+        # Cancel button entered
+        if not question:
+            easygui.msgbox("Wrong Input. Filed requires an answers",
+                           "No answer")
+
+        # Upper string boundary
+        elif len(question) > upper:
+            easygui.msgbox("Wrong Input. Please enter less than 20 letters",
+                           "Number to high")
+
+        # Lower string boundary
+        elif len(question) < lower:
+            easygui.msgbox("Wrong Input. Please more than 1 letters",
+                           "Number to low")
+
+        # Input correct
+        else:
+            return question.title()
+
+
+# Pre Stored Monster Cards dictionary
+monster_cards = {
+    "Stoneling":
+        {"Strength": 7,
+         "Speed": 1,
+         "Stealth": 25,
+         "Cunning": 15},
+    "Vexscream":
+        {"Strength": 1,
+         "Speed": 6,
+         "Stealth": 21,
+         "Cunning": 19},
+    "Dawnmirage":
+        {"Strength": 5,
+         "Speed": 15,
+         "Stealth": 18,
+         "Cunning": 22},
+    "Blazegolem":
+        {"Strength": 15,
+         "Speed": 20,
+         "Stealth": 23,
+         "Cunning": 6},
+    "Websnake":
+        {"Strength": 7,
+         "Speed": 15,
+         "Stealth": 10,
+         "Cunning": 5},
+    "Moldvine":
+        {"Strength": 21,
+         "Speed": 18,
+         "Stealth": 14,
+         "Cunning": 5},
+    "Vortexwing":
+        {"Strength": 19,
+         "Speed": 13,
+         "Stealth": 19,
+         "Cunning": 2},
+    "Rotthing":
+        {"Strength": 16,
+         "Speed": 7,
+         "Stealth": 4,
+         "Cunning": 12},
+    "Froststep":
+        {"Strength": 14,
+         "Speed": 14,
+         "Stealth": 17,
+         "Cunning": 4},
+    "Wispghoul":
+        {"Strength": 17,
+         "Speed": 19,
+         "Stealth": 3,
+         "Cunning": 2}
+}
+
+while True:
+    # Variable setting
+    value = True
+
+    # User enters search
+    searching = char_boundary(1, 20,
+                              "What Monster Card are you looking for? ")
+
+    # Loop to access all dictionary items
+    for name_card, item_card in monster_cards.items():
+
+        # If search is in monster card name
+        if searching in name_card:
+            easygui.buttonbox(f"The Monster Card named {name_card} has {item_card}")
+            value = False
+
+    # Warning message if search not in monster cards
+    if value:
+        print("Sorry, input not a Monster Card")
