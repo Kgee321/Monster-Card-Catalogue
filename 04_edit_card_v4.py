517b6dcd46bd1590535ede34f0971477f519e942
Kgee321 <geek@middleton.school.nz>
2023-05-14 22:08:21 +1200

    Update 04_edit_card_v4.py

--- 04_edit_card_v4.py
+++ 04_edit_card_v4.py
@@ -1,9 +1,11 @@
 """ Component 3 -- Edit card (version 4)
 Trial 3
-Use a for in range loop and a separate list
-of attributes in order to change the
-attributes values.
-Code works but is unnecessary long.
+Doesn't use a single loops to change attribute
+level.
+This is because only 1 card being used so
+doesn't need a loop to access the card.
+Shortest and most efficient code
+This code will be used in my future program.
 Written by Katelyn
 Created on the 7/05/2023
 """
@@ -35,9 +37,6 @@ edit_card = {
          "Cunning": 15},
 }
 
-# Attributes list
-attributes = ["Strength", "Speed", "Stealth", "Cunning"]
-
 # Loop for editing the card
 while True:
 
@@ -51,50 +50,31 @@ while True:
                                            "Stealth", "Cunning",
                                            "Exit"])
 
-    # If card not changing
+    # Setting value of the original edit card name
+    original_name = next(iter(edit_card))
+
+    # User wants to leave exit card program
     if what_edit == "Exit":
 
         # Happy message
         easygui.msgbox("Ok!", "Changes complete")
         break
 
-    # Loop for access dictionary values
-    for original_name, original_value in edit_card.items():
+    # Changing name
+    elif what_edit == "Name":
 
-        # Changing name
-        if what_edit == "Name":
+        # Asking what user want to change name to
+        edit_name = easygui.enterbox("Enter the new card name: ",
+                                     "Editing card name").title()
 
-            # Asking what user want to change name to
-            edit_name = easygui.enterbox("Enter the new card name: ",
-                                         "Editing card name").title()
+        # Adding name change to final dictionary
+        edit_card[edit_name] = edit_card.pop(original_name)
 
-            # Adding name change to final dictionary
-            edit_card[edit_name] = original_value
+    else:
 
-            # Deleting old card and ending loop
-            del edit_card[original_name]
-            break
+        # Asking for new level
+        edit_level = easygui.integerbox(f"Enter the new level of {what_edit}",
+                                        f"{what_edit} new level")
 
-        # Button if an attribute being changed
-        else:
-
-            # Loop for finding changing attribute value
-            for num in range(len(attributes)):
-
-                # Attribute being used
-                using = attributes[num]
-
-                # If correct attribute found
-                if using == what_edit:
-
-                    # Asking for new level
-                    edit_level = easygui.integerbox(f"Enter the new level of {using}",
-                                                    f"{using} new level")
-
-                    # Adding level change
-                    edit_card[original_name][using] = edit_level
-                    break
-
-                # If attribute not the one wanting to use
-                else:
-                    continue
+        # Adding level change
+        edit_card[original_name][what_edit] = edit_level
